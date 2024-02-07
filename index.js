766b4aa6e28dc1d787ae6f157656aa37c5b3fffb
baljor3 <baljor20@gmail.com>
2024-02-06 20:43:44 -0800

    adding functions

--- index.js
+++ index.js
@@ -191,6 +191,105 @@ app.all( "/admin", ( req, res ) => {
 		return res.redirect( "login" );
 	}
 } );
+
+app.post("/createFunctions",(req,res)=>{
+	let sql =`
+	CREATE OR REPLACE FUNCTION post_thread(
+		in_t_subject VARCHAR(120),
+		in_t_forum VARCHAR(18),
+		in_p_username VARCHAR(18),
+		in_p_text VARCHAR(1500),
+		in_p_country_code CHAR(2)
+	)
+	RETURNS INT AS $$
+	DECLARE new_post_id INT;
+	BEGIN
+		INSERT INTO Posts(
+			t_subject, t_forum, p_username, p_text, p_country_code)
+		VALUES(
+			in_t_subject, in_t_forum, in_p_username, in_p_text, in_p_country_code);
+	
+		SELECT currval(pg_get_serial_sequence('Posts', 'p_post_id')) INTO new_post_id;
+	
+		RETURN new_post_id;
+	END;
+	$$ LANGUAGE plpgsql;
+	
+	
+	-- post in a thread function
+	CREATE OR REPLACE FUNCTION post_reply(
+		in_p_thread_id INT,
+		in_p_username VARCHAR(18),
+		in_p_text VARCHAR(1500),
+		in_p_country_code CHAR(2)
+	)
+	RETURNS INT AS $$
+	DECLARE fresh_user INT := 1;
+	DECLARE new_post_id INT := -1;
+	BEGIN
+		INSERT INTO Posts(
+			p_thread_id, p_username, p_text, p_country_code)
+		VALUES(
+			in_p_thread_id, in_p_username, in_p_text, in_p_country_code);
+		SELECT currval(pg_get_serial_sequence('Posts', 'p_post_id')) INTO new_post_id;
+		-- increments post number
+		UPDATE posts
+		SET t_post_num = t_post_num + 1,
+		WHERE p_post_id = in_p_thread_id;
+		RETURN new_post_id;
+	END;
+	$$ LANGUAGE plpgsql;
+	
+	-- delete a post function
+	CREATE OR REPLACE FUNCTION delete_post(
+		in_p_post_id INT
+	)
+	RETURNS VOID AS $$
+	DECLARE found_thread_id INT;
+	BEGIN
+		--get thread_id
+		SELECT p_thread_id FROM Posts WHERE p_post_id = in_p_post_id INTO found_thread_id;
+		-- change post number
+		UPDATE posts
+		SET t_post_num = t_post_num - 1
+		WHERE p_post_id = in_p_post_id
+		OR p_thread_id = found_thread_id;
+		-- remove replies
+		DELETE FROM Replies WHERE parent_id = in_p_post_id OR reply_id = in_p_post_id;
+		-- remove reports
+		DELETE FROM Reports WHERE r_post_id = in_p_post_id;
+		-- remove post
+		DELETE FROM Posts WHERE p_post_id = in_p_post_id;
+	END;
+	$$ LANGUAGE plpgsql;
+	
+	-- delete a user and all posts
+	CREATE OR REPLACE FUNCTION delete_user(
+		in_username VARCHAR(18)
+	)
+	RETURNS VOID AS $$
+	BEGIN
+		-- delete all posts
+		PERFORM "delete_post"(p.p_post_id)
+		FROM Posts p
+		WHERE p.p_username = in_username;
+		-- delete forum
+		--DELETE FROM forums
+		--WHERE f_owner = in_username;
+		-- delete user
+		DELETE FROM Users
+		WHERE username = in_username;
+	END;
+	$$ LANGUAGE plpgsql;
+	`
+	db.query(sql,(err,result)=>{
+		if(err){
+			res.send(err)
+		}else{
+			res.send(result)
+		}
+	})
+})
 app.post("/createTables",(req,res)=>{
 	let sql = `CREATE TABLE Users(
 		username VARCHAR(18) PRIMARY KEY,
